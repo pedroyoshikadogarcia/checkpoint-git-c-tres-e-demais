dd88318f574cd5be610f94740fb632ea8663ca82
thiag <thiagomalufhof@gmail.com>
2026-03-17 11:50:35 -0300

    adição de comentario das funções idade e nome - Thiago Maluf

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include<stdio.h>// importação da biblioteca
 
-int main(){ 
+int main(){// Função principal -- Nomes 
     printf ("nome dos integrantes");
     printf ("Aluno 1: Pedro Yoshikado Garcia");
     printf ("Aluno 2: Pedro Andreassa Zamai");
     printf ("Aluno 3: Thiago Maluf Hofmann");
-
+        // Idades
     printf("Idades dos Integrantes");
     printf("Aluno 1: 21");
     printf("Aluno 2: 18");
