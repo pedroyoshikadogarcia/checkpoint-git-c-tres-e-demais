4a3210ffc870d8ccb52260ba5d18ba6f56b0e911
thiag <thiagomalufhof@gmail.com>
2026-03-17 11:36:53 -0300

    coloquei aluno 3 - Thiago Maluf

--- main.c
+++ main.c
@@ -4,4 +4,5 @@ int main(){
     printf ("nome dos integrantes");
     printf ("Aluno 1: Pedro Yoshikado Garcia");
     printf ("Aluno 2: Pedro Andreassa Zamai");
+    printf ("Aluno 3: Thiago Maluf Hofmann");
 }
