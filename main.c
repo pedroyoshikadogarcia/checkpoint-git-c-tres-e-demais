459100b71024284ab7304d87b8fec4a2d3de910e
pedroyoshikadogarcia <pedroyoshikado@gmail.com>
2026-03-17 11:38:53 -0300

    Criei a exibição da idade dos integrantes - Pedro Yoshikado Garcia

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
-#include<stdio.h>
+#include<stdio.h> 
 
-int main(){
+int main(){ 
     printf ("nome dos integrantes");
     printf ("Aluno 1: Pedro Yoshikado Garcia");
     printf ("Aluno 2: Pedro Andreassa Zamai");
     printf ("Aluno 3: Thiago Maluf Hofmann");
+
+    printf("Idades dos Integrantes");
 }
